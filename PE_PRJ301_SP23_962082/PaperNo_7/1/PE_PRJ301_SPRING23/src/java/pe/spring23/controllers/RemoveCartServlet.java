17e800e0124f66f1b7453562d613c9293fe25ef4
Do-Thomas <94061934+Do-Thomas@users.noreply.github.com>
2023-04-01 18:33:15 +0700

    Add files via upload
    PE_Spring 23_ GioHang

---
+++ PE_PRJ301_SP23_962082/PaperNo_7/1/PE_PRJ301_SPRING23/src/java/pe/spring23/controllers/RemoveCartServlet.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.spring23.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import pe.spring23.shopping.CART;
+import pe.spring23.shopping.DAO;
+
+/**
+ *
+ * @author Asus
+ */
+public class RemoveCartServlet extends HttpServlet {
+
+    private static final String ERROR = "error.jsp";
+    private static final String SUCCESS = "viewCart.jsp";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String url = ERROR;
+        try {
+            String id = request.getParameter("productID");
+            HttpSession session = request.getSession();
+            CART cart = (CART) session.getAttribute("CART");
+            if (cart != null) {
+                if (cart.getCart().containsKey(id)) {
+                    cart.remove(id);
+
+                }
+                if (cart.getCart().size() == 0) {
+                    cart = null;
+
+                }
+                session.setAttribute("CART", cart);
+                url = SUCCESS;
+            }
+
+        } catch (Exception e) {
+            log("Error at RemoveCartController: " + e.toString());
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
